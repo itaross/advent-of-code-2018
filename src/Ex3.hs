433b4f857db895990d648bd85424f3dcdbc3c406
alessandro <alessandro.rossignoli@oncode.it>
2018-12-20 01:59:40 +0100

    Day3 pt2 solution

--- src/Ex3.hs
+++ src/Ex3.hs
@@ -55,15 +55,35 @@ solEx3 = do
   let listedRectangles = concatMap generateListFromRectangle rectangles
 
   -- part 1
-  print . length . filter (>1) . fmap length . group . sort . fmap getDupleFromTriple $ listedRectangles
+  print . length . filter (>1) . fmap length . group . sort . fmap getDupleFrom4ple $ listedRectangles
 
   -- part 2
+  -- sort the 4ples based on the second and third parameter (x, y)
+  let sortedTuples = sortOn getDupleFrom4ple $ listedRectangles
+  -- find the non overlapping rectangles
+  let nonOverlappingPoints = fmap head . filter ((==1) . length) . groupBy (\t u -> (getDupleFrom4ple t) == (getDupleFrom4ple u)) . sortOn getDupleFrom4ple $ listedRectangles
+  -- group the 4ples based on uid
+  let groupedPoints = groupBy (\t u -> (getFirstFrom4ple t) == (getFirstFrom4ple u)) . sortOn getFirstFrom4ple $ nonOverlappingPoints
+  -- get only those that have length = width * height ==> all the non overlapping points equal to the size of the rectangle
+  -- that is all (only one) full non-overlapping rectangle
+  let findAnswer = getFirstFrom4ple . head . head . filter (\f -> (getFourthFrom4ple (head f)) `equalityInt` (length f) ) $ groupedPoints
+  print findAnswer
 
-generateListFromRectangle :: Rectangle -> [(Integer, Integer, Integer)]
+
+generateListFromRectangle :: Rectangle -> [(Integer, Integer, Integer, Integer)]
 generateListFromRectangle (Rectangle {..}) = do
   xPoint <- [x..x + width - 1]
   yPoint <- [y..y + height - 1]
-  return (uid, xPoint, yPoint)
+  return (uid, xPoint, yPoint, width * height)
 
-getDupleFromTriple :: (Integer, Integer, Integer) -> (Integer, Integer)
-getDupleFromTriple (_, a, b) = (a, b)
+getDupleFrom4ple :: (Integer, Integer, Integer, Integer) -> (Integer, Integer)
+getDupleFrom4ple (_, a, b, _) = (a, b)
+
+getFirstFrom4ple :: (Integer, Integer, Integer, Integer) -> Integer
+getFirstFrom4ple (a, _, _, _) = a
+
+getFourthFrom4ple :: (Integer, Integer, Integer, Integer) -> Integer
+getFourthFrom4ple (_, _, _, a) = a
+
+equalityInt :: Integer -> Int -> Bool
+equalityInt a b = a == (toInteger b)
